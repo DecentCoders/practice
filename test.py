34551ef70b0a607d422a3d57a641e194dc1b43ad
Hridoy Hawladar <decentcoderz@gmail.com>
2025-12-23 20:43:43 +0800

    use of end=''

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
-suspects = ['A','B','C','D']
-for x in suspects:
-    if(x!='A')+(x=='C')+(x=='D')+(x!='D')==3:
-        print('The theif is: ',x)
-        break
+numA=int(input("Enter the first number: "))
+numB  =int( input ('Enter the second number:'))
+sum = numA+ numB
+average =sum/2
+print('The sum is:',sum ,end="")
+print('The average is:',average)
