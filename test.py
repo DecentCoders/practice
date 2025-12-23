6f19169632dda21f6256cf4e581a5e77c04f6eff
Hridoy Hawladar <decentcoderz@gmail.com>
2025-12-23 22:27:27 +0800

    money exchange

--- test.py
+++ test.py
@@ -1,6 +1,15 @@
-numA=int(input("Enter the first number: "))
-numB  =int( input ('Enter the second number:'))
-sum = numA+ numB
-average =sum/2
-print('The sum is:',sum ,end="")
-print('The average is:',average)
+# numA=int(input("Enter the first number: "))
+# numB  =int( input ('Enter the second number:'))
+# sum = numA+ numB
+# average =sum/2
+# print('The sum is:',sum ,end="")
+# print('The average is:',average)
+money = int(input("Enter the amount of money: "))
+m50 = money//50
+money = money%50
+m5 = money//5
+money = money%5
+m1 = money
+print("The number of 50 notes: ",m50)
+print("The number of 5 notes: ", m5)
+print("The number of 1 notes: ",m1)
