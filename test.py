7e551467872f3dc2a563f9e79c72915cf8a3c8db
Hridoy Hawladar <decentcoderz@gmail.com>
2025-12-25 20:42:47 +0800

    #finding month

--- test.py
+++ test.py
@@ -4,12 +4,19 @@
 # average =sum/2
 # print('The sum is:',sum ,end="")
 # print('The average is:',average)
-money = int(input("Enter the amount of money: "))
-m50 = money//50
-money = money%50
-m5 = money//5
-money = money%5
-m1 = money
-print("The number of 50 notes: ",m50)
-print("The number of 5 notes: ", m5)
-print("The number of 1 notes: ",m1)
+# money = int(input("Enter the amount of money: "))
+# m50 = money//50
+# money = money%50
+# m5 = money//5
+# money = money%5
+# m1 = money
+# print("The number of 50 notes: ",m50)
+# print("The number of 5 notes: ", m5)
+# print("The number of 1 notes: ",m1)
+
+
+#finding month
+m = int(input("Enter the number of month you want to find: "))
+months = "JanFebMarAprMayJunJulAugSepOctNovDec"
+pos = (m-1)*3
+print('The month is ',months[pos:pos+3])
