1f50a0f07d34e5f100548419d5f7ceaaa7603a6a
Hridoy Hawladar <decentcoderz@gmail.com>
2026-01-29 22:58:26 +0800

    check wining function added

--- test.py
+++ test.py
@@ -12,5 +12,17 @@ def print_board(board):
     print(f" {board[6]} | {board[7]} | {board[8]}  ")
     print("\n")
 
-# Check if a player has won
-print_board(init_board())
+def check_win(board, player):
+    # All winning combinations (3 in a row/column/diagonal)
+    win_combinations = [
+        [0,1,2], [3,4,5], [6,7,8],  # Horizontal
+        [0,3,6], [1,4,7], [2,5,8],  # Vertical
+        [0,4,8], [2,4,6]             # Diagonal
+    ]
+    for combo in win_combinations:
+        if board[combo[0]] == board[combo[1]] == board[combo[2]] == player:
+            return True
+    return False
+
+# Check if the board is full (draw)
+
