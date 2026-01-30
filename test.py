17682dd4d7b35d1629c86d6bad2cc0305eca4356
Hridoy Hawladar <decentcoderz@gmail.com>
2026-01-30 22:04:28 +0800

    loop counter

--- test.py
+++ test.py
@@ -1,11 +1,2 @@
-score = int(input("Enter your Score: "))
-if score >= 90:
-    print("Your Grade A")
-elif score> 80:
-    print("Your Grade B")
-elif score> 70 :
-    print("Your Grade C")
-elif score> 60:
-    print("Your Grade D")
-else:
-    print("sorry you failed!")
+for i in range(1,101):
+    print(i)
