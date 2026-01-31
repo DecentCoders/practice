90483f3f0553816b23fdf23eafa9cdbd3a56c4ba
Hridoy Hawladar <decentcoderz@gmail.com>
2026-01-31 17:26:09 +0800

    learning append

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
-nums = [12,12,3,2,3,4,5,5,6]
-total = 0
-for i in nums:
-    total+=i
-print(total)
-average = total/len(nums)
-print(f"{average:.2f}")
-    
+names = []
+firstname = input("Enter first name: ")
+middlename = input("Enter middle name:")
+secondname= input("Enter the second name: ")
+names.append(firstname)
+print(names)
