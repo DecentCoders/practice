5e6a395c6a81d9ae328bc87a9febb8dbe8e193ce
Hridoy Hawladar <decentcoderz@gmail.com>
2026-01-31 17:20:05 +0800

    reading a list and doing total, average

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
-fruits = ["mango","strawberry","watermelon","apple","grapes"]
-for i in fruits:
-    print(i)
+nums = [12,12,3,2,3,4,5,5,6]
+total = 0
+for i in nums:
+    total+=i
+print(total)
+average = total/len(nums)
+print(f"{average:.2f}")
+    
