3ae85444a5ca4cc85538ad6f53f14f99d8529afa
Hridoy Hawladar <decentcoderz@gmail.com>
2026-02-01 23:04:49 +0800

    formatting

--- test.py
+++ test.py
@@ -25,3 +25,9 @@ window_width = window.winfo_width()
 window_height = window.winfo_height()
 screen_width = window.winfo_screenwidth()
 screen_height = window.winfo_screenheight()
+
+window_x = int((screen_width/2) - (window_width/2))
+window_y = int((screen_height/2) - (window_height/2))
+
+#format "(w)x(h)+(x)+(y)"
+window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
