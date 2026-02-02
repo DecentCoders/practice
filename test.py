a0691fd1d6252cc2059e0a55ffb937fc98bb6c71
Hridoy Hawladar <decentcoderz@gmail.com>
2026-02-02 23:56:36 +0800

    body

--- test.py
+++ test.py
@@ -7,3 +7,14 @@ button_values = [
     ["1", "2", "3", "+"],
     ["0", ".", "√", "="]
 ]
+right_symbols = ["÷", "×", "-", "+", "="]
+top_symbols = ["AC", "+/-", "%"]
+
+row_count = len(button_values) #5
+column_count = len(button_values[0]) #4
+
+color_light_gray = "#D4D4D2"
+color_black = "#1C1C1C"
+color_dark_gray = "#505050"
+color_orange = "#FF9500"
+color_white = "white"
