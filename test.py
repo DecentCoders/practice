576fff51ab10b2dc8d5cc464e9090df358594923
Hridoy Hawladar <decentcoderz@gmail.com>
2026-02-06 23:44:32 +0800

    even numbers using while

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
-for i in range (0,101):
-    if i%2 == 0:
-        print(i, end=",")
+# for i in range (0,101):
+#     if i%2 == 0:
+#         print(i, end=",")
+num = 0
+while num<= 100:
+    if num%2 == 0:
+        print(num,end=",")
+    num += 1
