9e9883da4289bfabb2623eabe4b8220abeb5c354
Hridoy Hawladar <decentcoderz@gmail.com>
2026-02-07 16:53:48 +0800

    printed the welcome mssg

--- test.py
+++ test.py
@@ -1,1 +1,20 @@
-questions =[]
+import time
+def kbcGame():
+    questions =[
+    ["What is the name of the capital of Bangladesh?","Chittagong","Dhaka","Khulna","Rajshahi",2],
+    ["What is the national flower of Bangladesh?","Rose","Sapla","Orange","Gandha",2],
+    ["How many colors are there in the national flag of Bangladesh?","One","Two","Three","Four",2],
+    ["When is Bangladesh's Independence Day celebrated?","March 26","December 16","February 21","April 14",1],
+    ["In what year did the Liberation War of Bangladesh begin?","1969","1970","1971","1972",3],
+    ["Who was the first president of Bangladesh?","Sheikh Mujibur Rahman","Tajuddin Ahmed","Syed Nazrul Islam","Khandaker Mushtaq Ahmed",1],
+    ["How many square kilometers is the total area of ​​Bangladesh?"," 1,47,570","1,50,000","1,40,000","1,60,000",1],
+    ["How many members are there in the National Parliament of Bangladesh?","300","350","400","250",2],
+    ["Which is the highest award of Bangladesh?","Ekushey Padak","Freedom Award","National Film Award","Bangla Academy Award",2],
+    ["Which is the largest river of Bangladesh?","Padma","Meghna","Brahmaputra","Karnaphuli",3]
+    ]
+
+levels = ['1000','2000','3000','4000','5000','6000','7000','8000','9000','10000']
+money_won = 0
+print("------Who will be millionaire------")
+time.sleep(1)
+
