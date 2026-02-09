eda419ca911712188553b35d9b5b9e69cef1d2a9
Hridoy Hawladar <decentcoderz@gmail.com>
2026-02-09 23:59:14 +0800

    added check winner

--- test.py
+++ test.py
@@ -21,3 +21,17 @@ def set_tile(row, column):
 
     #check winner
     check_winner()
+    
+def check_winner():
+    global turns, game_over
+    turns += 1
+
+    #horizontally, check 3 rows
+    for row in range(3):
+        if (board[row][0]["text"] == board[row][1]["text"] == board[row][2]["text"]
+            and board[row][0]["text"] != ""):
+            label.config(text=board[row][0]["text"]+" is the winner!", foreground=color_yellow)
+            for column in range(3):
+                board[row][column].config(foreground=color_yellow, background=color_light_gray)
+            game_over = True
+            return
