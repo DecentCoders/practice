6b02b5c3ca16603e94e324c84a5c753e650e794a
Hridoy Hawladar <decentcoderz@gmail.com>
2026-02-09 23:51:35 +0800

    Guest List practice

--- test.py
+++ test.py
@@ -1,7 +1,5 @@
-temps = [15, 22, 18, 25, 30, 12, 27]
-warm_temps =[]
-for t in temps:
-    if t > 20:
-        warm_temps.append(t)
-for temp in warm_temps:
-    print(f'Safe temperature: {temp}')
+names =['Hridoy','Hawladar','Rifat','Sajib','Riad']
+with open ('guests.txt', 'w') as file:
+    for name in names:
+        file.write(name + '\n')   
+print("File 'guests.txt' has been created.")
