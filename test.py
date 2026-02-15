3554fe8485308b528bcfa20c49781cac26201580
Hridoy Hawladar <decentcoderz@gmail.com>
2026-02-15 23:29:07 +0800

    file writing

--- test.py
+++ test.py
@@ -1,7 +1,4 @@
-text = input("Enter a word or sentence: ")
-lowertext = text.lower()
-vowel_count = 0
-for i in lowertext:
-    if i == "a" or i == "e" or i == "i" or i == "o" or i == "u":
-        vowel_count += 1
-print("the number of vowels is: ", vowel_count)
+cities = ["London", "Paris", "New York", "Tokyo", "Sydney"]
+with open("cities.txt","w") as file:
+    for city in cities:
+        file.write(city +"\n")
