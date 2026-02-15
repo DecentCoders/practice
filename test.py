0069b2d69e5c879281c787f8c6a3676f848a0248
Hridoy Hawladar <decentcoderz@gmail.com>
2026-02-15 23:44:13 +0800

    used loop to count lines

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 with open("cities.txt","r+") as file:
     lines = file.readlines()
-    line_count = len(lines)
-    print(line_count)        
+    line_count = 0
+    for line in lines:
+        line_count+=1
+    print(line_count)     
