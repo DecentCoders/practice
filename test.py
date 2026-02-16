c1984c18e5049b82d91baec5184c2ee3627138ac
Hridoy Hawladar <decentcoderz@gmail.com>
2026-02-16 17:02:37 +0800

    trying to take input in a dictionary

--- test.py
+++ test.py
@@ -1,5 +1,19 @@
-with open("cities.txt", "r") as file:
-    lines = file.readlines()
-    for line in lines: 
-        if line.startswith("N"):
-            print(line)
+def calc_score(score):
+    grade = ""
+    return grade
+name = ""
+id = 0
+score = 0
+student_info = {
+    "Name":name,
+    "ID":id,
+    "Score": score,
+    }
+for i in range(0,3):
+    name = input("Enter your name: ")
+    if name == "quite":
+        print("Quiting...")
+        break
+    
+print(name)
+    
