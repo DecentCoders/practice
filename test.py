8c881467f1878a9a0cdf7122b379625c1b1c4094
Hridoy Hawladar <decentcoderz@gmail.com>
2026-02-19 17:06:12 +0800

    use of enumerate looop

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
-a = 10
-b =5
-print("A")if a<b else print("B")
+marks =[43,343,35,6,3,32,54]
+# index = 0
+# for mark in marks:
+#     print(mark)
+#     if index == 3:
+#         print("hello..")
+#     index +=1
+for index, mark in enumerate(marks):
+    print(mark)
+    if index ==3:
+        print("hellooo")
