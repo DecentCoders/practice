0aa60f037deaf9964dd267cc302da173d4403cbd
Hridoy Hawladar <decentcoderz@gmail.com>
2026-02-20 01:57:44 +0800

    use of __name__

--- test.py
+++ test.py
@@ -1,11 +1,2 @@
-marks =[43,343,35,6,3,32,54]
-# index = 0
-# for mark in marks:
-#     print(mark)
-#     if index == 3:
-#         print("hello..")
-#     index +=1
-for index, mark in enumerate(marks):
-    print(mark)
-    if index ==3:
-        print("hellooo")
+from main import test
+test()
