7d6df6c288e97e9470d83f0a87f198acd96b2b62
Hridoy Hawladar <decentcoderz@gmail.com>
2026-02-20 23:58:07 +0800

    added for two digits

--- secretCode/main.py
+++ secretCode/main.py
@@ -1,6 +1,4 @@
-main_code =input("Enter your Code: ")
-if len(main_code)<3:
-    secret_code = reversed(main_code)
-else:
-    secret_code = main_code
+main_code = input("Enter your Code: ")
+if len(main_code) <3:
+    secret_code = ''.join(reversed(main_code))
 print(secret_code)
