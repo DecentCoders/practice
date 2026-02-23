78084c2e5a2d9cc07b37b37a1809625606c221c6
Hridoy Hawladar <decentcoderz@gmail.com>
2026-02-23 23:53:40 +0800

    refactor: update secret code generation logic

--- secretCode/main.py
+++ secretCode/main.py
@@ -2,5 +2,7 @@ main_code = input("Enter your Code: ")
 if len(main_code) <3:
     secret_code = main_code[::-1]
 else:
-    secret_code = "".random().join(main_code).random()
+    random_1= "bdd"
+    random_2 ="dss"
+    secret_code = random_1+ main_code[1:]+main_code[:1]+ random_2
 print(secret_code)
