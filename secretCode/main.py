36858f640b6bf15a9e883a88b503e20a6ba7d624
Hridoy Hawladar <decentcoderz@gmail.com>
2026-02-23 23:46:09 +0800

    refactor: update banana calculation logic and add input validation

--- secretCode/main.py
+++ secretCode/main.py
@@ -1,4 +1,6 @@
 main_code = input("Enter your Code: ")
 if len(main_code) <3:
-    secret_code = ''.join(reversed(main_code))
+    secret_code = main_code[::-1]
+else:
+    secret_code = "".random().join(main_code).random()
 print(secret_code)
