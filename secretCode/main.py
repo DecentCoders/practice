74c12654879307c9eed13728cf3313db90d0b060
Hridoy Hawladar <decentcoderz@gmail.com>
2026-02-25 00:27:12 +0800

    refactor: improve user input handling and menu display in main loop

--- secretCode/main.py
+++ secretCode/main.py
@@ -1,6 +1,5 @@
 import random
 import string
-main_code = input("Enter your code:")
 def encode(main_code):
     if len(main_code)<3:
         secret_code = main_code[::-1]
@@ -10,22 +9,42 @@ def encode(main_code):
         secret_code = random1 + main_code[1::]+main_code[:1]+ random2
     return secret_code
 def decode(code):
-    if len(code)<3:
+    if len(code)<6:
         decoded = code[::-1]
     else:
         middle = code[3:-3]
         decoded = middle[-1]+middle[:-1]
     return decoded
-    
+encoded_code = False
 while True:
-    encode(main_code)
-    print("1:See your encoded code\n 2: Decode your code\n 3:Quite")
-    options = input("Enter your choice: ")
-    if options == 1:
-        encoded_code = encode(main_code)
-        print(encoded_code)
-    elif options ==2:
-        encoded_code = encode(main_code)
-        decoded_code = decode(encoded_code)
-    break
+    if encoded_code == False:
+        main_code = input("Enter your code or 'Quite' to exite :").strip()
+        if main_code.lower()=='quite':
+            print("Exiting program..")
+            break
+    print("\n====Menu====")
+    print("1: See your encoded code")
+    print("2: Decode your code")
+    print("3: Encode another code")
+    print("4: Quite")
+    
+    options = input("Enter your choice (1/2/3): ").strip()
+    
+    if options == '1':
+        encoded = encode(main_code)
+        print(f"Encoded: {encoded}")
+        encoded_code = True
+    elif options =='2':
+        encoded = encode(main_code)
+        decoded_code = decode(encoded)
+        print(f'Decoded code: {decoded_code}')
+        encoded_code = True
+
+    elif options =='3':
+        encoded_code = False
+    elif options == '4':
+        print("Quiting..")
+        break
+    else:
+        print("Invalid choice! Please enter 1,2,3")
     
