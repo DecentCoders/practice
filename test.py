bc070cf7c49dbcf0319dd10161ee7a37b6c3bdc2
Hridoy Hawladar <decentcoderz@gmail.com>
2026-02-25 00:58:49 +0800

    refactor: restructure os_practice function for improved directory handling

--- test.py
+++ test.py
@@ -1,5 +1,30 @@
 import os 
-folders = os.listdir("test")
-for folder in folders:
-    print(folder)
-    print(os.listdir(f"test/{folder}"))
+def os_practice():
+    print(os.getcwd())
+    print(os.listdir("icpc/"))
+    a= input("Enter a directory: ")
+    if os.path.exists(a):
+        print("path exists")
+    else:
+        print("path didn't exist")
+    new_dir = input("Enter new directory name: ")
+    try:
+        os.mkdir(new_dir)
+        print("New directory created successfully")
+    except:
+        if os.path.exists(new_dir):
+            print("Directory already exists..")
+        else:
+            print("something went wrong")
+    os.makedirs("abc/bc/c")
+    os.path.abspath("kbc/index.py")
+    print(os.path.split(dir))
+    print(os.path.basename(dir))
+    
+# ---------------------------
+dir = input("Enter the path:")
+
+
+
+
+
