628ad155df07189179eb3176f6b32925a93fa83d
Hridoy Hawladar <decentcoderz@gmail.com>
2026-02-26 23:11:39 +0800

    practicing os remove()

--- test.py
+++ test.py
@@ -1,31 +1,6 @@
-import os 
-def os_practice():
-    print(os.getcwd())
-    print(os.listdir("icpc/"))
-    a= input("Enter a directory: ")
-    if os.path.exists(a):
-        print("path exists")
-    else:
-        print("path didn't exist")
-    new_dir = input("Enter new directory name: ")
-    try:
-        os.mkdir(new_dir)
-        print("New directory created successfully")
-    except:
-        if os.path.exists(new_dir):
-            print("Directory already exists..")
-        else:
-            print("something went wrong")
-    os.makedirs("abc/bc/c")
-    os.path.abspath("kbc/index.py")
-    print(os.path.split(dir))
-    print(os.path.basename(dir))
-    
-    dir = input("Enter the path:")
-    print(os.name)
-    print(os.cpu_count())
-    print(os.getpid())
-# ---------------------------
-
-
-
+import os
+try:
+    file_name = input("enter file name:")
+    os.remove(f"test/{file_name}")
+except Exception as e:
+    print(e)
