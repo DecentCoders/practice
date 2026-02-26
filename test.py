bd1ddb5f6df475d875ba9ea3c77705593b4c43e8
Hridoy Hawladar <decentcoderz@gmail.com>
2026-02-26 23:43:45 +0800

    use of rmdir()

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import os
 try:
-    file_name = input("enter file name:")
-    os.remove(f"test/{file_name}")
+    file_name = input("enter directory name:")
+    os.rmdir(file_name)
 except Exception as e:
     print(e)
