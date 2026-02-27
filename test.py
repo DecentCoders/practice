7c33684246bb1d86b8325c3b00893f3c61b36f95
Hridoy Hawladar <decentcoderz@gmail.com>
2026-02-27 23:30:53 +0800

    use of walk()

--- test.py
+++ test.py
@@ -1,11 +1,21 @@
 import os
-directory = input("Enter a directory you want to search in: ")
-file_name = input("Enter the filename you want to search for: ")
-if not file_name:
-    print("The file doesn't exitst")
-else:
-    for root , dirs, files in os.walk('.'):
-        for file in files:
-            if file ==(file_name):
-                full_path = os.path.join(root,file_name)
-                print(f'Found the file,{full_path}')
+
+def count_total_files(directory):
+    """
+    Count the total number of files (excluding directories) in a given directory and all its subdirectories.
+    """
+    total_file_count = 0
+
+    for root, dirs, files in os.walk(directory):
+        total_file_count += len(files) 
+    return total_file_count
+
+if __name__ == "__main__":
+    target_directory = "./your_target_dir" 
+    try:
+        count = count_total_files(target_directory)
+        print(f"Total number of files in {target_directory} (including subdirectories): {count}")
+    except FileNotFoundError:
+        print(f"Error: The directory {target_directory} does not exist.")
+    except NotADirectoryError:
+        print(f"Error: {target_directory} is not a valid directory (it is a file).")
