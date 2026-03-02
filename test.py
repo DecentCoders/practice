5c4e29ba511aeb8e4bb578d58ff1d1e22d2318ee
Hridoy Hawladar <decentcoderz@gmail.com>
2026-03-03 01:05:31 +0800

    use of map()

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
-double = lambda x:x*2
-print(double(5))
+def cube (a):
+    return a*a
+l=[1,2,4,5,6,3,6]
+new_l = list(map(cube,l))
+print(new_l)
+def square(a):
+    return a*a
+l=[4,3,6,3,6,3]
+new_l = list(map(square,l))
+print(new_l)
