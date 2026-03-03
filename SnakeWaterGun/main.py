75fa762218c66b037401c70388844d1067a7ff56
Hridoy Hawladar <decentcoderz@gmail.com>
2026-03-03 17:25:05 +0800

    finished using only if-else

--- SnakeWaterGun/main.py
+++ SnakeWaterGun/main.py
@@ -10,6 +10,12 @@ def snake_water_game():
     computer_choice = random.choice(["w","s","g"])
     print(f"you choosen {choice}")
     print(f'Computer chosed {computer_choice}')
+    if choice == computer_choice:
+        print("it's a draw..")
+    elif (choice == "s" and computer_choice =="w") or (choice == "g" and computer_choice =="s") or ( choice == "w"  and computer_choice == "g"):
+        print("Congratulation, You won..")
+    else:
+        print("Computer won, better luck next time")
     
-    
-snake_water_game()
+if __name__ == "__main__":
+    snake_water_game()
