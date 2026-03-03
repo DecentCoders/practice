526be5b13f59d1c96c6f96521e250a732a284b72
Hridoy Hawladar <decentcoderz@gmail.com>
2026-03-03 17:18:16 +0800

    added computer choice

--- SnakeWaterGun/main.py
+++ SnakeWaterGun/main.py
@@ -1,6 +1,15 @@
 import random
 def snake_water_game():
-    options = { "W":"water","S":"snake", "G":"gun"}
+    options = { "w":"water","s":"snake", "g":"gun"}
     print("-------------Snake Water Gun-------------------")
+    print(options)
+    choice =input("Enter your choice: ")
+    if choice not in options:
+        print("Invalid choice..Please choose s, w, or g.")
+        return
+    computer_choice = random.choice(["w","s","g"])
+    print(f"you choosen {choice}")
+    print(f'Computer chosed {computer_choice}')
+    
     
 snake_water_game()
