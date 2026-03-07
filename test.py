6abb341012058457e658c08736a4ff38e26cd89c
Hridoy Hawladar <decentcoderz@gmail.com>
2026-03-07 23:46:29 +0800

    basics of class

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
-class person:
-    name = "Hridoy"
-    occupation = "Developer"
-    net_worth = 10
-a = person()
-print(a.name, a.occupation)
+# class person:
+#     name = "Hridoy"
+#     occupation = "Developer"
+#     net_worth = 10
+#     def test(self):
+#         print(f"{self.name} is a {self.occupation}")
+# a = person()
+# a.test()
