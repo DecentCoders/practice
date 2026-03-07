d5ce29dddacd5975c5093b05b2e2ad298abdd9fe
Hridoy Hawladar <decentcoderz@gmail.com>
2026-03-07 23:49:42 +0800

    test

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-# class person:
-#     name = "Hridoy"
-#     occupation = "Developer"
-#     net_worth = 10
-#     def test(self):
-#         print(f"{self.name} is a {self.occupation}")
-# a = person()
-# a.test()
+class person:
+    name = "Hridoy"
+    occupation = "Developer"
+    net_worth = 10
+    def test(self):
+        print(f"{self.name} is a {self.occupation}")
+a = person()
+a.test()
