f1e54b8172b3512f22ceaf15acaaaf94dbc274cb
Hridoy Hawladar <decentcoderz@gmail.com>
2026-03-09 23:56:34 +0800

    aded the exception for options

--- libraryManagement/main.py
+++ libraryManagement/main.py
@@ -1,5 +1,10 @@
 print("----Welcome to Hridoy's Library----")
 while True:
     options =["Purchase Book", "Sell Book","Available books","Exit"]
+    choices = [0,1,2,3]
     for i in range(len(options)):
-        print(f'{options[i]}')
+        print(f'{i}:{options[i]}')
+    user_choice = input("Enter your choice(0-3): ")
+    if user_choice not in choices:
+        print("Invalid choice, try again!. \n")       
+        
