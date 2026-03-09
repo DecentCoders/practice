be1517e3c33b856a3e0c0e93d33da5ccb8c088d1
Hridoy Hawladar <decentcoderz@gmail.com>
2026-03-09 23:52:06 +0800

    initializing library management

---
+++ libraryManagement/main.py
@@ -0,0 +1,5 @@
+print("----Welcome to Hridoy's Library----")
+while True:
+    options =["Purchase Book", "Sell Book","Available books","Exit"]
+    for i in range(len(options)):
+        print(f'{options[i]}')
