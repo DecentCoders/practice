c74ec065da2771bed9736a74f3cb8dabfc21415d
Hridoy Hawladar <decentcoderz@gmail.com>
2026-03-10 14:06:54 +0800

    got issue with this

--- libraryManagement/books.py
+++ libraryManagement/books.py
@@ -1,2 +1,6 @@
+import colors
 def avail_books():
-    print("-----Available Books----")
+    print(f"{colors.GREEN}-----Available Books----{colors.RESET}")
+    with open ("Available_Books.txt","r") as file:
+        avail_books_list = file.readlines()
+        print(avail_books_list.strip())
