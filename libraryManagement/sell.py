bf907163cbab5c59d0949d4b617214e07643633b
Hridoy Hawladar <decentcoderz@gmail.com>
2026-03-10 14:59:56 +0800

    working initially

--- libraryManagement/sell.py
+++ libraryManagement/sell.py
@@ -9,22 +9,33 @@ def sell_books():
         return
     if not book_name_check:
         print('Book name cant be empty or only spaces \n')
-    page_number = input("Enter the page number: ".strip())
-    if not page_number:
-        print('Page number cant be empty')
+        return
+   
     try:
-        page_numbers = int(page_number)
+        page_number = int(input("Enter the page number: ".strip()))
+        if not page_number:
+            print('Page number cant be empty')
+            return
     except:
         print("Page number must be a integer number \n")
         return
     
-    if page_numbers < 0: 
+    if page_number < 0: 
         print("Page number can't be negative!")
+    try:
+        price = int(input("Price (USD): "))
+    except Exception as e:
+        print("price must be a integer number")
+        return
+    if price <0:
+        print("Price cant be negative number")
+        return
         
     try:
         with open ('Available_Books.txt','a+') as file:
-            book_entry= f'{book_name}-Page Number: {page_number} \n'
-            for line in file:
+            book_entry= f'{book_name}-Page Number: {page_number} - Price: {price}USD \n'
+            avail_test= file.readlines()
+            for line in avail_test:
                 if line == book_name:
                     print("The book already exists.")
                     return
