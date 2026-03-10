084d49a0787369f411357ce7e55ef0eb9cb7e8d2
Hridoy Hawladar <decentcoderz@gmail.com>
2026-03-10 13:50:32 +0800

    added color codes

--- libraryManagement/main.py
+++ libraryManagement/main.py
@@ -1,9 +1,16 @@
 import books
 import purchase
 import sell
+#Color codes 
+RED = "\033[1;31m"      
+GREEN = "\033[1;32m"     
+BLUE = "\033[1;34m"      
+YELLOW = "\033[1;33m"    
+PURPLE = "\033[1;35m"    
+RESET = "\033[0m"        
 print("----Welcome to Hridoy's Library----")
 while True:
-    options =["Purchase Book", "Sell Book","Available books","Exit"]
+    options =["Available books","Purchase Book", "Sell Book","Exit"]
     print("----Menu----")
     for i,option in enumerate(options):
         print(f'{i+1}: {option}')
@@ -18,11 +25,11 @@ while True:
         print("\n Invalid choice, Please select from menu\n")
         continue
     elif user_choice == 1:
-        purchase.purchase_books()
-    elif user_choice == 2:
-        sell.sell_books()
-    elif user_choice ==3:
         books.avail_books()
+    elif user_choice == 2:
+        purchase.purchase_books()
+    elif user_choice ==3:
+        sell.sell_books()
     else:
         print("Exiting...")
         break
