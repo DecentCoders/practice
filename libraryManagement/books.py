8fdc49ed54cd503f73acd4ed2e9c084140a49fa6
Hridoy Hawladar <decentcoderz@gmail.com>
2026-03-10 14:34:38 +0800

    got some eror i dont know where

--- libraryManagement/books.py
+++ libraryManagement/books.py
@@ -1,6 +1,10 @@
 import colors
 def avail_books():
     print(f"{colors.GREEN}-----Available Books----{colors.RESET}")
-    with open ("Available_Books.txt","r") as file:
-        avail_books_list = file.readlines()
-        print(avail_books_list.strip())
+    try:  
+        with open ("Available_Books.txt","r") as file:
+            avail_books_list = file.readlines()
+            for line in avail_books_list:
+                print(line.strip())
+    except:
+        print("something is wrong")
--- libraryManagement/sell.py
+++ libraryManagement/sell.py
@@ -1,2 +1,26 @@
+import colors
 def sell_books():
-    print("")
+    print(f'{colors.GREEN}selling corner..{colors.RESET}')
+    try:
+        book_name = input("Enter the book name: ")
+    except Exception as e:
+        print(e)
+        return
+    if not book_name:
+        print('Book name cant be empty')
+    page_number = input("Enter the page number: ")
+    try:
+        page_numbers = int(page_number)
+    except:
+        print("Page number must be a integer number")
+        return
+    
+    if page_numbers < 0: 
+        print("Page number can't be negative!")
+        
+    try:
+        with open ('Available_Books.txt','w+') as file:
+            book_entry= f'{book_name}-Page Number {page_number} \n'
+            file.write(book_entry)
+    except Exception as e:
+        print(e)
