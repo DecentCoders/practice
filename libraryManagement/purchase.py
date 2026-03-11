b9ef13acc1ec8b0f1f47c6b8b2b8b39162bf93ba
Hridoy Hawladar <decentcoderz@gmail.com>
2026-03-11 21:14:26 +0800

    again stuck

--- libraryManagement/purchase.py
+++ libraryManagement/purchase.py
@@ -4,8 +4,14 @@ def purchase_books():
     try:
         with open("Available_Books.txt", 'r+') as file:
             books = file.readlines()
+            books_filterd = books[1:]
             print("Book name ~ Page ~ Price (USD)")
-            for index, book in enumerate(books):
-                print(f'{index+1} {book[index+1].strip()}')
+            for index, book in enumerate(books_filterd,start=1):
+                print(f'{colors.YELLOW} {index}:{colors.RESET} {book.strip()}')
+            choose_book = int(input('Enter he name/index of the book you want to buy: '))
+            book_choosed = books_filterd[choose_book]
+            print(f'{colors.PURPLE}You choose to buy {book_choosed} {colors.RESET}')
     except Exception as e:
         print(e)
+if __name__ ==" __main__":
+    purchase_books()
