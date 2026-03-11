6ff1875ed95d45d6f0e6467c6c210d463e8598ec
Hridoy Hawladar <decentcoderz@gmail.com>
2026-03-11 20:54:09 +0800

    something is wrong

--- libraryManagement/purchase.py
+++ libraryManagement/purchase.py
@@ -1,4 +1,11 @@
 import colors
 def purchase_books():
     print(f"{colors.GREEN}----Purchase  books---{colors.RESET}")
-    
+    try:
+        with open("Available_Books.txt", 'r+') as file:
+            books = file.readlines()
+            print("Book name ~ Page ~ Price (USD)")
+            for index, book in enumerate(books):
+                print(f'{index+1} {book[index+1].strip()}')
+    except Exception as e:
+        print(e)
